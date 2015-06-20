8666b342a8f8857ea1fb61c4fa5452e3eaf5aee5
N1amr <n1amr1@gmail.com>
2015-06-20 03:32:42 +0200

    Chapter 12: Dialogs - Add DatePicker to DialogFragment

--- app/src/main/java/com/android/n1amr/criminalintent/controllers/details/dialog/DatePickerFragment.java
+++ app/src/main/java/com/android/n1amr/criminalintent/controllers/details/dialog/DatePickerFragment.java
@@ -4,13 +4,15 @@ import android.app.AlertDialog;
 import android.app.Dialog;
 import android.app.DialogFragment;
 import android.os.Bundle;
+import android.view.View;
 
 import com.android.n1amr.criminalintent.R;
 
 public class DatePickerFragment extends DialogFragment {
     @Override
     public Dialog onCreateDialog(Bundle savedInstanceState) {
+        View view = getActivity().getLayoutInflater().inflate(R.layout.dialog_date, null);
         return new AlertDialog.Builder(getActivity()).setTitle(R.string.date_picker_title).setPositiveButton(android.R.string.ok, null).setNegativeButton(android.R.string.cancel,
-                null).create();
+                null).setView(view).create();
     }
 }
