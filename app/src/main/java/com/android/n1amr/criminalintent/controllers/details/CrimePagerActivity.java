8246dd8e036f7f74de3baa79352ab160abb7de3c
N1amr <n1amr1@gmail.com>
2015-06-20 02:42:03 +0200

    Chapter 11: ViewPager

--- app/src/main/java/com/android/n1amr/criminalintent/controllers/details/CrimePagerActivity.java
+++ app/src/main/java/com/android/n1amr/criminalintent/controllers/details/CrimePagerActivity.java
@@ -7,7 +7,6 @@ import android.os.Bundle;
 import android.support.v13.app.FragmentStatePagerAdapter;
 import android.support.v4.view.ViewPager;
 import android.support.v7.app.ActionBarActivity;
-import android.util.Log;
 
 import com.android.n1amr.criminalintent.R;
 import com.android.n1amr.criminalintent.model.Crime;
