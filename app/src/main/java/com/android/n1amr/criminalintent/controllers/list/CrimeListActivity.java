415502cf4a81b2d381baa967cdcd07a29f3654cc
N1amr <n1amr1@gmail.com>
2015-06-27 11:06:14 +0200

    Load/Store Crimes Data

--- app/src/main/java/com/android/n1amr/criminalintent/controllers/list/CrimeListActivity.java
+++ app/src/main/java/com/android/n1amr/criminalintent/controllers/list/CrimeListActivity.java
@@ -1,10 +1,13 @@
 package com.android.n1amr.criminalintent.controllers.list;
 
 import android.app.Fragment;
-import android.view.Menu;
+import android.os.Bundle;
+import android.os.PersistableBundle;
+import android.util.Log;
 
-import com.android.n1amr.criminalintent.R;
+import com.android.n1amr.criminalintent.CriminalIntentJSONSerializer;
 import com.android.n1amr.criminalintent.controllers.SingleFragmentActivity;
+import com.android.n1amr.criminalintent.model.CrimeLab;
 
 /**
  * Created by N1amr on 2015/06/19.
@@ -15,6 +18,4 @@ public class CrimeListActivity extends SingleFragmentActivity {
     protected Fragment createFragment() {
         return new CrimeListFragment();
     }
-
-
 }
--- app/src/main/java/com/android/n1amr/criminalintent/model/CrimeLab.java
+++ app/src/main/java/com/android/n1amr/criminalintent/model/CrimeLab.java
@@ -2,18 +2,34 @@ package com.android.n1amr.criminalintent.model;
 
 
 import android.content.Context;
+import android.util.Log;
+
+import com.android.n1amr.criminalintent.CriminalIntentJSONSerializer;
 
 import java.util.ArrayList;
 import java.util.UUID;
 
 public class CrimeLab {
+    private static final String TAG = "CrimeLab";
+    private static final String FILENAME = "crimes.json";
+
+    private ArrayList<Crime> mCrimes;
+    private CriminalIntentJSONSerializer mSerializer;
+
     private static CrimeLab sCrimeLab;
     private Context mAppContext;
-    private ArrayList<Crime> mCrimes;
 
     private CrimeLab(Context appContext) {
         mAppContext = appContext;
-        mCrimes = new ArrayList<Crime>();
+        mSerializer = new CriminalIntentJSONSerializer(mAppContext, FILENAME);
+
+        try {
+            mCrimes = mSerializer.loadCrimes();
+        } catch (Exception e) {
+            mCrimes = new ArrayList<Crime>();
+            Log.e(TAG, "Error loading crimes: ", e);
+        }
+
 //        for (int i = 0; i < 5; i++) {
 //            Crime crime = new Crime();
 //            crime.setTitle("Crime #" + i);
@@ -44,4 +60,14 @@ public class CrimeLab {
         mCrimes.add(crime);
     }
 
+    public boolean saveCrimes() {
+        try {
+            mSerializer.saveCrimes(mCrimes);
+            Log.d(TAG, "crimes saved to file");
+            return true;
+        } catch (Exception e) {
+            Log.e(TAG, "Error saving crimes: ", e);
+            return false;
+        }
+    }
 }
